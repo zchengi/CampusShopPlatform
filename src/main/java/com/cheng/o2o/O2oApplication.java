368bc477c5281871af96cf7b427c7d048ee1b1b4
cheng <792702352@qq.com>
2018-04-19 15:47:51 +0800

    Spring Boot Init

---
+++ src/main/java/com/cheng/o2o/O2oApplication.java
@@ -0,0 +1,12 @@
+package com.cheng.o2o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2oApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2oApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cheng/o2o/O2oApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cheng.o2o;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class O2oApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
